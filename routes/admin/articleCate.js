31244e4d40e52e1e3e0a87f34b37b6068c9100f6
‘czx’ <2331938324@qq.com>
2025-06-13 17:54:08 +0800

    内容管理开始

--- routes/admin/articleCate.js
+++ routes/admin/articleCate.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {getUnix} = require('../../model/tools')
 const ArticleCateModel = require('../../model/articleCateModel')
+const  mongoose  = require('../../model/core')
 
 var routes = express.Router()
 
@@ -15,13 +16,20 @@ routes.get('/add',async(req,res)=>{
     var topCateList = await ArticleCateModel.find({'pid':'0'})
 
     res.render('admin/articleCate/add.html',{
-     cateList:topCateList
+        cateList:topCateList
     })
 
  })
 
- routes.get('/doAdd',async(req,res)=>{
-   
+ routes.post('/doAdd',async(req,res)=>{
+   if(req.body.pid != 0) {
+    req.body.pid = new mongoose.Types.ObjectId(req.body.pid)
+   }
+   req.body.add_time = getUnix()
+
+   var result  = new ArticleCateModel(req.body)
+   await result.save()
+   res.redirect(`/${req.app.locals.adminPath}/articleCate`)
  })
 
 module.exports  = routes
